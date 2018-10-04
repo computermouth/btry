cb8e8ae5468a1e72404b7c81babaa23aaca7d8df
computermouth <computermouth@crunchbangplusplus.org>
2018-10-04 06:28:10 +0000

    Color code refactor, but blue doesn't work right

--- main.c
+++ main.c
@@ -3,8 +3,6 @@
 #include <string.h>
 #include <unistd.h>
 
-#include "global.h"
-
 #define MIN(A, B)	((A) < (B) ? (A) : (B))
 
 /* --------- XEMBED and systray stuff */
@@ -14,6 +12,13 @@
 
 static int trapped_error_code = 0;
 static int (*old_error_handler) (Display *, XErrorEvent *);
+
+#define BC 0 // dark green
+#define BD 1 // dark red
+#define FC 2 // white
+#define FD 3 // white
+
+XColor colors[4];
 
 static int
 error_handler(Display * display, XErrorEvent * error) {
@@ -63,52 +68,92 @@ send_systray_message(Display* dpy, long message, long data1, long data2, long da
 	}
 }
 
-void print_colors(){
+void init_colors(){
 	
-	int i;
-	size_t len = (sizeof (x_colors) / sizeof (const char *));
+	// orange background charging
+	colors[BC].flags = DoRed | DoGreen | DoBlue;
+	colors[BC].red   = 0xF5 * 255;
+	colors[BC].green = 0xB0 * 255;
+	colors[BC].blue  = 0x65 * 255;
 	
-	printf("\nColors:\n\n");
+	// blue background discharging
+	colors[BD].flags = DoRed | DoGreen | DoBlue;
+	colors[BD].red   = 0xAE * 255;
+	colors[BD].green = 0xD6 * 255;
+	colors[BD].blue  = 0xF1 * 255;
 	
-	for (i = 0; i < len; i++ )
-		printf("  %d '%s'\n", i, x_colors[i]);
+	// black foreground charging
+	colors[FC].flags = DoRed | DoGreen | DoBlue;
+	colors[FC].red   = 0x00 * 255;
+	colors[FC].green = 0x00 * 255;
+	colors[FC].blue  = 0x00 * 255;
 	
-	printf("\n");
+	// black forground discharging
+	colors[FD] = colors[FC];
 	
 }
 
-void print_usage(char * bin){
+void print_usage(){
 	
 	printf("\n\
 btry v0.1 (20180101) system tray battery monitor\n\
 \n\
-Usage: %s [-hc] [-b COLOR] [-f COLOR] [-B COLOR] [-F COLOR]\n\
+Usage: btry [-h] [-b FFFFFF] [-f FFFFFF] [-B FFFFFF] [-F FFFFFF]\n\
 \n\
   -h --help          print this dialog\n\
-  -c --colors        print all valid color names\n\
   -b --bg-charge     background color while charging\n\
   -f --fg-charge     foreground color while charging\n\
   -B --bg-discharge  background color while discharging\n\
-  -F --fg-discharge  foreground color while discharging\n\
-\n",
-	bin);
+  -F --fg-dischasrge  foreground color while discharging\n\
+\n");
 	
 }
 
-int validate_color(char * color, int c_index){
+int parse_color(char * color, int c_i){
 	
-	int i;
-	size_t len = (sizeof (x_colors) / sizeof (const char *));
-	
-	for (i = 0; i < len; i++ ) {
+	// fail if flag wasn't passed a real color
+	if (strlen(color) != 6){
 		
-		if ( ! strcmp(x_colors[i], color) ){
-			d_colors[c_index] = i;
-			return 0;
-		}
+		printf("E: invalid color '%s', must be 6 hex digits -- FFFFFF\n", color);
+		print_usage();
+		return 1;
 	}
 	
-	return 1;
+	uint8_t values[6];
+	
+	for (int i = 0; i < 6; i++ ) {
+		
+		if (color[i] >= '0' && color[i] <= '9'){
+			values[i] = color[i] - '0';
+			continue;
+		}
+		
+		if(color[i] >= 'A' && color[i] <= 'F'){
+			values[i] = color[i] - 'A' + 10;
+			continue;
+		}
+		
+		if(color[i] >= 'a' && color[i] <= 'f'){
+			values[i] = color[i] - 'a' + 10;
+			continue;
+		}
+		
+		return 1;
+	}
+	
+	// convert 6 0-15 numbers to 3 0-65535 numbers
+	// hopefully xlib converts against floor values
+	// because this will turn 0-255 -> 0-65280
+	colors[c_i].red   = (values[0] * 0x10 + values[1]) * 0x100;
+	colors[c_i].green = (values[2] * 0x10 + values[3]) * 0x100;
+	colors[c_i].blue  = (values[4] * 0x10 + values[5]) * 0x100;
+	
+	printf("%d\n", c_i);
+	printf("%d - %d - %d - %d - %d - %d\n", values[0], values[1], values[2], values[3], values[4], values[5]);
+	printf("r: %d\ng: %d\nb: %d\n", colors[c_i].red, colors[c_i].green, colors[c_i].blue);
+	printf("\n");
+	
+	return 0;
 }
 
 int parse_args(int argc, char **argv){
@@ -117,25 +162,19 @@ int parse_args(int argc, char **argv){
 	for(i = 1; i < argc; i++){
 		
 		if ( ! strcmp(argv[i], "-h") || ! strcmp(argv[i], "--help") ) {
-			print_usage(argv[0]);
-			return 1;
-		} else if ( ! strcmp(argv[i], "-c") || ! strcmp(argv[i], "--colors") ) {
-			print_colors();
+			print_usage();
 			return 1;
 		} else if ( ! strcmp(argv[i], "-b") || ! strcmp(argv[i], "--bg-charge") ) {
 			
 			// ensure required argument
 			if (argc == i + 1){
-				printf("E: %s requires argument [COLOR]\n", argv[i]);
-				print_usage(argv[0]);
+				printf("E: %s requires argument [COLOR] -- FFFFFF\n", argv[i]);
+				print_usage();
 				return 1;
 			}
 			
-			// fail if flag wasn't passed a real color
-			if (validate_color(argv[i + 1], BGC)){
-				
-				printf("E: invalid color '%s'\n", argv[i + 1]);
-				print_colors();
+			if (parse_color(argv[i + 1], BC)) {
+				printf("E: failed to parse colors\n");
 				return 1;
 			}
 			
@@ -146,15 +185,12 @@ int parse_args(int argc, char **argv){
 			// ensure required argument
 			if (argc == i + 1){
 				printf("E: %s requires argument [COLOR]\n", argv[i]);
-				print_usage(argv[0]);
+				print_usage();
 				return 1;
 			}
 			
-			// fail if flag wasn't passed a real color
-			if (validate_color(argv[i + 1], FGC)){
-				
-				printf("E: invalid color '%s'\n", argv[i + 1]);
-				print_colors();
+			if (parse_color(argv[i + 1], FC)) {
+				printf("E: failed to parse colors\n");
 				return 1;
 			}
 			
@@ -165,15 +201,12 @@ int parse_args(int argc, char **argv){
 			// ensure required argument
 			if (argc == i + 1){
 				printf("E: %s requires argument [COLOR]\n", argv[i]);
-				print_usage(argv[0]);
+				print_usage();
 				return 1;
 			}
 			
-			// fail if flag wasn't passed a real color
-			if (validate_color(argv[i + 1], BGD)){
-				
-				printf("E: invalid color '%s'\n", argv[i + 1]);
-				print_colors();
+			if (parse_color(argv[i + 1], BD)) {
+				printf("E: failed to parse colors\n");
 				return 1;
 			}
 			
@@ -184,15 +217,12 @@ int parse_args(int argc, char **argv){
 			// ensure required argument
 			if (argc == i + 1){
 				printf("E: %s requires argument [COLOR]\n", argv[i]);
-				print_usage(argv[0]);
+				print_usage();
 				return 1;
 			}
 			
-			// fail if flag wasn't passed a real color
-			if (validate_color(argv[i + 1], FGD)){
-				
-				printf("E: invalid color '%s'\n", argv[i + 1]);
-				print_colors();
+			if (parse_color(argv[i + 1], FD)) {
+				printf("E: failed to parse colors\n");
 				return 1;
 			}
 			
@@ -200,14 +230,13 @@ int parse_args(int argc, char **argv){
 			continue;
 		} else {
 			printf("E: unrecognized flag '%s'\n", argv[i]);
-			print_usage(argv[0]);
+			print_usage();
 			return 1;
 		}
 		
 	}
 	
 	return 0;
-	
 }
 
 /* ------------ Regular X stuff */
@@ -219,9 +248,12 @@ main(int argc, char **argv) {
 	//~ Colormap cm;
 	//~ XVisualInfo vi;
 	Display *dpy;
+	//~ GC gc;
 	int screen;
 	Window root, win;
 	int rc = 0;
+	
+	init_colors();
 	
 	if (argc > 1) // no args -> defaults
 		rc = parse_args(argc, argv);
@@ -236,49 +268,42 @@ main(int argc, char **argv) {
 	root = RootWindow(dpy, screen);
 	if(!XGetWindowAttributes(dpy, root, &wa))
 		return 1;
+	
 	width = height = MIN(wa.width, wa.height);
 	
-	XColor final_colors[4];
 	XColor throwaway;
 	
-	//~ vi = XGetVisualInfo(dpy, wa.depth, TrueColor, &vinfo);
-	//~ cm = XCreateColormap(dpy, win, )
+	throwaway.flags= DoRed | DoGreen | DoBlue;
+	throwaway.red = 65535;
+	throwaway.green = 32767;
+	throwaway.blue = 0;
 	
-	//~ printf("%d, %d, %d, %d, %d\n", dpy, wa.colormap, x_colors[d_colors[BGC]], throwaway, bgc);
-	
-	return 0;
+	if(XAllocColor(dpy,wa.colormap,&throwaway) == 0){
+		printf("Failed to allocate color\n");
+		return 1;
+	}
 	
 	for(int i = 0; i < 4; i++){
-		if(!XLookupColor(dpy, wa.colormap, x_colors[d_colors[i]], &throwaway, final_colors + (i * sizeof(XColor)))){
-			printf("E: failed to look up %s\n", x_colors[d_colors[i]]);
+		if(XAllocColor(dpy, wa.colormap, &(colors[i])) == 0){
+			printf("Failed to allocate color\n");
+			return 1;
 		}
 	}
 	
-	printf("%x, %x, %x, %x\n", final_colors[0].pixel, final_colors[1].pixel, final_colors[2].pixel, final_colors[3].pixel);
+	win = XCreateSimpleWindow(dpy, root, 0, 0, width, height, 0, 0, colors[0].pixel);
 	
-	//~ if(!XLookupColor(dpy, wa.colormap, x_colors[d_colors[BGC]], &throwaway, &bgc)) {
-		//~ printf("E: failed to look up BGC\n");
-		//~ return 1;
-	//~ }
+	//~ gc = XCreateGC(dpy, win, NULL, NULL);
 	
-	// https://tronche.com/gui/x/xlib/graphics/drawing/
-	//~ if (! XLookupColor(dpy, colormap, ""))
+	//~ final_colors[0] = throwaway;
+	
+	//~ printf("%x\n", final_colors[0].pixel);
 	
 	// this is probably wrong
 	// XDrawPoint(dpy, win, root, x, y);
-	
-	//~ XColor xcolour;
-
-	//~ xcolour.red = 32000; xcolour.green = 65000; xcolour.blue = 32000;
-	//~ xcolour.flags = DoRed | DoGreen | DoBlue;
-	//~ XAllocColor(d, cmap, &xcolour);
 
 	//~ XSetForeground(d, gc, xcolour.pixel);
 	//~ XFillRectangle(d, w, gc, 0, 0, winatt.width, 30);
 	//~ XFlush(d);
-	
-	/* create window */
-	win = XCreateSimpleWindow(dpy, root, 0, 0, width, height, 0, 0, 0x708090);
 	
 	send_systray_message(dpy, SYSTEM_TRAY_REQUEST_DOCK, win, 0, 0); // pass win only once
 	XMapWindow(dpy, win);
